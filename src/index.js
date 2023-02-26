3170ba67c525739ef0951fb3dee0c19419892be2
EtoZheLamia <Andariel2175@yandex.ru>
2023-02-26 17:27:01 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const OPEN_BRACKETS = [];
+  const BRACKETS_PAIR = new Map()
+  bracketsConfig.forEach(element => {
+    OPEN_BRACKETS.push(element[0])
+    BRACKETS_PAIR.set(element[1], element[0])
+  })
+  let stack = []
+  for (let i = 0; i < str.length; i++) {
+    const currentSymbol = str[i];
+    let topElement = stack[stack.length - 1] 
+    if (BRACKETS_PAIR.get(currentSymbol) && BRACKETS_PAIR.has(currentSymbol) && topElement === currentSymbol) {
+      stack.pop()
+    }else if (OPEN_BRACKETS.includes(currentSymbol)) {
+      stack.push(currentSymbol);
+    } else {
+      if (stack.length === 0) {
+            return false
+          }
+      if (BRACKETS_PAIR.get(currentSymbol) === topElement) {
+            stack.pop()
+          } else {
+            return false
+          } 
+    }
+  }
+  return stack.length === 0;
 }
